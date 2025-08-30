64fd09aeda0c70e662905127aa0d9bbc192f8f3b
minjoon-98 <4kmj54321@gmail.com>
2025-08-30 14:50:34 +0900

    [Fix] Subject.java에 Set 임포트 추가

--- src/main/java/com/a404/duckonback/entity/Subject.java
+++ src/main/java/com/a404/duckonback/entity/Subject.java
@@ -16,6 +16,7 @@ import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
