0cc4abf5414734410b5df9bd0caf88ed850496ff
se0hyun <be8shyn@gmail.com>
2026-01-20 16:02:50 +0900

    feat: 태그 검색 서비스 로직 구현

--- src/main/java/com/a404/duckonback/service/TagSearchService.java
+++ src/main/java/com/a404/duckonback/service/TagSearchService.java
@@ -1,6 +1,7 @@
 package com.a404.duckonback.service;
 
 import com.a404.duckonback.dto.TrendingTagDTO;
+import com.a404.duckonback.entity.Tag;
 
 import java.time.Duration;
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface TagSearchService {
     void logSearchKeyword(String keyword);
     List<TrendingTagDTO> getTrendingTags(Duration range, int size);
+    List<Tag> searchTags(String keyword, int limit);
 }
