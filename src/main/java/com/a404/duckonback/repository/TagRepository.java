8f660834e7c779d0246e4b2df3593e78821b5fa6
se0hyun <be8shyn@gmail.com>
2026-01-20 16:01:26 +0900

    feat: TagRepository에 접두어/부분일치 검색 메서드 추가

--- src/main/java/com/a404/duckonback/repository/TagRepository.java
+++ src/main/java/com/a404/duckonback/repository/TagRepository.java
@@ -2,9 +2,13 @@ package com.a404.duckonback.repository;
 
 import com.a404.duckonback.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Optional<Tag> findByTagName(String tagName);
+    List<Tag> findByTagNameStartingWithIgnoreCase(String prefix, Pageable pageable);    
+    List<Tag> findByTagNameContainingIgnoreCase(String keyword, Pageable pageable);
 }
