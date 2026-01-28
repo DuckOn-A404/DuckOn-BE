3127db313cfc2a5316de962cb665bea51cfbb4d3
se0hyun <be8shyn@gmail.com>
2026-01-28 15:02:10 +0900

    feat: 신고 리포트 조회 기능

--- src/main/java/com/a404/duckonback/domain/admin/controller/AdminController.java
+++ src/main/java/com/a404/duckonback/domain/admin/controller/AdminController.java
@@ -9,6 +9,8 @@ import com.a404.duckonback.domain.admin.service.AdminService;
 import com.a404.duckonback.domain.admin.dto.AdminUserListDTO;
 import com.a404.duckonback.domain.artist.service.ArtistService;
 import com.a404.duckonback.domain.meme.service.MemeRankingBatchService;
+import com.a404.duckonback.domain.report.dto.ReportDTO;
+import com.a404.duckonback.domain.report.service.ReportService;
 import com.a404.duckonback.domain.user.service.EngagementBatchService;
 import com.a404.duckonback.domain.user.service.UserRankService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -34,6 +36,7 @@ public class AdminController {
     private final EngagementBatchService engagementBatchService;
     private final MemeRankingBatchService memeRankingBatchService;
     private final AdminService adminService;
+    private final ReportService reportService;
 
     @Operation(summary = "아티스트 등록 (JWT 필요O)", description = "새로운 아티스트를 등록합니다.")
     @PostMapping("/artists")
@@ -82,5 +85,15 @@ public class AdminController {
         PageResponse<AdminUserListDTO> userList = adminService.getAdminUserList(page, size);
         return ResponseEntity.ok(ApiResponseDTO.success(SuccessCode.ADMIN_GET_USER_LIST_SUCCESS, userList));
     }
+
+    @Operation(summary = "신고 목록 조회 (JWT 필요O)", description = "신고 목록을 조회합니다.")                                                                                       
+    @GetMapping("/reports")                                                                                                                                                           
+    public ResponseEntity<ApiResponseDTO<PageResponse<ReportDTO>>> getAllReports(
+        @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {                                                                                
+        PageResponse<ReportDTO> reportDTOs = reportService.getAllReports(page, size);                                                                                                                                                                                                                                                                             
+        return ResponseEntity.ok(ApiResponseDTO.success(SuccessCode.ADMIN_GET_REPORT_LIST_SUCCESS, reportDTOs));                                                                      
+    }       
 
 }
