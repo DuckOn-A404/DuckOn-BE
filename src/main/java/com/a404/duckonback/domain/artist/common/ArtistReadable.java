2cf1d932bda151b90436c5fc0e8c112535760df4
Yejin <qja086351@naver.com>
2026-02-02 17:59:24 +0900

    feat: 아티스트 공동 처리를 위한 ArtistReadable interface 생성

---
+++ src/main/java/com/a404/duckonback/domain/artist/common/ArtistReadable.java
@@ -0,0 +1,8 @@
+package com.a404.duckonback.domain.artist.common;
+
+public interface ArtistReadable {
+    Long getId();
+    String getNameEn();
+    String getNameKr();
+    String getImgUrl();
+}
