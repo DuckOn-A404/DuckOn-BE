8567399253342105dea5eb0fdaa49acfbcf4dd4c
Yejin <qja086351@naver.com>
2026-02-04 18:20:22 +0900

    feat: S3Presigner Bean 추가

--- src/main/java/com/a404/duckonback/common/config/S3Config.java
+++ src/main/java/com/a404/duckonback/common/config/S3Config.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -20,12 +21,24 @@ public class S3Config {
     @Value("${S3_REGION}")
     private String region;
 
+    private StaticCredentialsProvider credentialsProvider(){
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return StaticCredentialsProvider.create(credentials);
+    }
+
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
+                .credentialsProvider(credentialsProvider())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(){
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider())
                 .build();
     }
 }
